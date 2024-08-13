4bcca79c13326b6a43f66dc77a57f810a64787ab
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2024-08-13 23:29:05 +0000

    Aula01

---
+++ aula01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program {
+    public static void Main(string[]args){
+        //Cadastras os dados de pessoa
+
+        Console.WriteLine("Cadastro de Pessoa");
+        Console.Write("Nome: ");
+        string nomePessoa = Console.ReadLine();
+
+        Console.Write("Idade: ");
+        int idadePessoa = int.Parse(Console.ReadLine());
+
+        Pessoa obj1 = new Pessoa(nomePessoa, idadePessoa);
+
+        Console.WriteLine(obj1);
+    }
+}
