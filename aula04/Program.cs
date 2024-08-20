7482a4a9f0c477ce75578cafd415138a2716edd7
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2024-08-20 23:31:07 +0000

    Codigo da aula 20-08-2024

---
+++ aula04/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program{
+    public static void Main (string[] args){
+        Pessoa objPessoa = new Pessoa("Gustavo");
+        Console.WriteLine("Antes do método (Classe)" + objPessoa.ToString());
+        AlteraNomeClasse (objPessoa);
+        Console.WriteLine("Depois do método (Classe):" +objPessoa.ToString());
+
+        PessoaStruct objPessoaStruct = new PessoaStruct("Adriana");
+        Console.WriteLine("Antes do método (Struct)" +objPessoaStruct);
+        AlteraNomeStruct(objPessoaStruct);
+        Console.WriteLine("Depois do método (Struct):" +objPessoaStruct.ToString());
+    }
+
+    public static void AlteraNomeClasse(Pessoa objAux){
+        objAux.Nome = "Laura";
+    }
+
+    public static void AlteraNomeStruct(PessoaStruct objAuxStruct){
+        objAuxStruct.NomeStruct = "Rogerio";
+    }
+}
